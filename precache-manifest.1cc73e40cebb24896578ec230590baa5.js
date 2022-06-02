eb96709c3313d69001029817b1aceb8ee261f37b
Tejas Gawali <tejasgawali.12345@gmail.com>
2022-06-02 22:52:31 +0530

    Updates

---
+++ precache-manifest.1cc73e40cebb24896578ec230590baa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef54533f3fb3236076df",
+    "url": "/TextUtils/static/css/main.d0b85b12.chunk.css"
+  },
+  {
+    "revision": "ef54533f3fb3236076df",
+    "url": "/TextUtils/static/js/main.ef54533f.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/TextUtils/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "17d3433a509bbb0f67ef",
+    "url": "/TextUtils/static/js/2.17d3433a.chunk.js"
+  },
+  {
+    "revision": "94fa866583836ad85178",
+    "url": "/TextUtils/static/js/runtime~main.94fa8665.js"
+  },
+  {
+    "revision": "88318f385009ff836b827ea553cf4de9",
+    "url": "/TextUtils/index.html"
+  }
+];
